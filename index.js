77bd255067ee1dc6372baf7dc1f762207d1feded
Ritu kukreja <104751465+Ritu0208@users.noreply.github.com>
2022-09-11 13:22:15 +0530

    validation updated

--- index.js
+++ index.js
@@ -18,10 +18,15 @@ function submitHandler(){
         showOutput(`Please Enter positive values`);
         outputBox.style.color='white';
     }else{
+        if (Number(initial) !== 0 && Number(quantity) !== 0 && Number(current) !== 0) {
         initial = Number(initialPrice.value);
         quantity = Number(numberOfStocks.value);
         current = Number(currentPrice.value);
         calculateProfitAndLoss(initial, quantity, current);   
+        } else {
+            showOutput("Values should be greater than 0.");
+            outputBox.style.color='white';
+        }
     }
 }
 function calculateProfitAndLoss(initial, quantity, current){
